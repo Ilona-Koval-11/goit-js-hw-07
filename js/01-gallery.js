836a69608235316c89bbc53988a23650d0ff6017
Ilona-Koval-11 <106178217+Ilona-Koval-11@users.noreply.github.com>
2022-11-08 17:51:18 +0200

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -14,10 +14,87 @@
 
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+/*import {basicLightbox} from './basicLightbox.js';*/
 console.log(galleryItems);
 
-const gallery = document.querySelector('.gallery');
+const divRef = document.querySelector('.gallery');
+
+function createGalleryMarkup(items){
+    return items.map(
+        (item) => `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image"
+        src="${item.previev}"
+        data-source="{item.original}"
+        alt="${item.description}"
+        />
+        </a>
+        </div>`
+    )
+    .join("");
+}
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+divRef.innerHTML = addGalleryMarkup;
+divRef.addEventListener("click", onImageClick);
+function onImageClick(evt){
+    blockStandartAction(evt);
+
+    if(evt.target.nodeName !== "IMG"){
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+    `);
+    instance.show();
+    divRef.addEventListener("keydown", (evt) =>{
+        if (evt.code === "Escape"){
+            instanse.close();
+        }
+    });
+}
+function blockStandartAction(evt){
+    evt.preventDefault();
+}
+
+
+/*const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup );
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
+function onGalleryContainerClick(evt){
+    evt.preventDefault();
+    const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${evt.target, dataset.sourse}" width="800" height="600">
+    </div>
+        `);
+    const  isGalleryImageElement = evt.target.classList.contains('gallery__image');
+    if(!isGalleryImageElement){
+        return;
+    }
+    instance.show();
+/*console.log(evt.target);*/
+/*}
+
+
+function createGalleryItemsMarkup (galleryItems){
+    return galleryItems
+    .map(({preview, original, description}) => 
+           ` <div class ="gallery__item">
+        <a class = "gallery__link" href="${original}">
+        <img class= "gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+       </div>`
+    ).join("");
+    
+}; */
+/*const gallery = document.querySelector('.gallery');
 const galleryMarkup = createGallery(galleryItems);
 gallery.insertAdjacentHTML('beforeend',galleryMarkup );
 gallery.addEventListener('click', onClick);
@@ -51,7 +128,7 @@ function onClick(evt){
     document.addEventListener('keydown', (evt) => {
         if (evt.key === 'Escape') instance.close();
     });
-}
+}*/
 
 /*function onClick(evt){
     evt.preventDefault();
